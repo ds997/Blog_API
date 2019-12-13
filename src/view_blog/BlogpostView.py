a31a4429206dfe88f3d6671719f24e2555485d93
Divyanshu Sachdeva <ds997@njit.edu>
2019-12-13 00:35:26 -0500

    Feature: PUT endpoint for case Users can only update their own blogpost and added auth for verifying users by id

--- src/view_blog/BlogpostView.py
+++ src/view_blog/BlogpostView.py
@@ -46,6 +46,29 @@ def get_one(blogpost_id):
     return custom_response(data, 200)
 
 
+@blogpost_api.route('/<int:blogpost_id>', methods=['PUT'])
+@Auth.auth_required
+def update(blogpost_id):
+    """
+    Update A Blogpost
+    """
+    req_data = request.get_json()
+    post = BlogPostModel.get_one_blogpost(blogpost_id)
+    if not post:
+        return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    if data.get('owner_id') != g.user.get('id'):
+        return custom_response({'error': 'permission denied'}, 400)
+
+    data, error = blogpost_schema.load(req_data, partial=True)
+    if error:
+        return custom_response(error, 400)
+    post.update(data)
+
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 200)
+
+
 def custom_response(res, status_code):
     """
   Custom Response Function
