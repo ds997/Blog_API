332d88a607a75c7964ad286e72e13e006d518669
Divyanshu Sachdeva <ds997@njit.edu>
2019-12-12 19:59:42 -0500

    Feature: Imported user_api as user_blueprint and registered the blueprint.

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 from .config import app_config
 from .model_blog import db, bcrypt
-
+from .view_blog.UserView import user_api as user_blueprint
 
 def create_app(env_name):
 
@@ -15,6 +15,8 @@ def create_app(env_name):
     bcrypt.init_app(app)
 
     db.init_app(app)
+
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
 
     @app.route('/', methods=['GET'])
     def index():
