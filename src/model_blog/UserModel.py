a1edf4da5b94205b93c5e2436a5c8c4c91766368
Divyanshu Sachdeva <ds997@njit.edu>
2019-12-12 19:10:27 -0500

    Feature: Added Flask bcrypt dependency for encrypting password and Code in UserModel to allow hashing of user's password

--- src/model_blog/UserModel.py
+++ src/model_blog/UserModel.py
@@ -1,6 +1,7 @@
 from marshmallow import fields, Schema
 import datetime
 from . import db
+from ..app import bcrypt
 
 "User Model"
 
@@ -23,6 +24,7 @@ class UserModel(db.Model):
         """
         self.name = data.get('name')
         self.email = data.get('email')
+        self.password = self.__generate_hash(data.get('password'))
         self.password = data.get('password')
         self.created_at = datetime.datetime.utcnow()
         self.modified_at = datetime.datetime.utcnow()
@@ -33,6 +35,8 @@ class UserModel(db.Model):
 
     def update(self, data):
         for key, item in data.items():
+            if key == 'password':
+                self.password = self.__generate_hash(value)
             setattr(self, key, item)
         self.modified_at = datetime.datetime.utcnow()
         db.session.commit()
@@ -40,6 +44,14 @@ class UserModel(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+    def __generate_hash(self, password):
+        return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
+
+        # add this new method
+
+    def check_hash(self, password):
+        return bcrypt.check_password_hash(self.password, password)
 
     @staticmethod
     def get_all_users():
