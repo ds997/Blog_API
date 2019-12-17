accaad36b19dcd957ffdc2d73b55f8d67b7a23e0
Divyanshu Sachdeva <ds997@njit.edu>
2019-12-17 03:10:42 -0500

    fix : using WSGI(Web Server Gateway Interface) server for production through gunicorn

--- run.py
+++ run.py
@@ -1,23 +1,11 @@
-# import os
-#
-# from src.app import create_app
-#
-# if __name__ == '__main__':
-#     env_name = os.getenv('FLASK_ENV')
-#     app = create_app(env_name)
-#     port = os.getenv('PORT')
-#     # run app
-#     app.run(host='0.0.0.0', port=port)
-#
-#     # run app
-#     app.run()
-
+# /run.py
 import os
-
 from src.app import create_app
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
 if __name__ == '__main__':
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
+    port = os.getenv('PORT')
     # run app
-    app.run()
+    app.run(host='0.0.0.0', port=5432)
